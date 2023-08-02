1e5ba7b8ba50c17a3f78cfe7bfdd5e3fee3210d1
ManuDv <m.alfaro@live.cl>
2023-08-02 16:32:38 -0400

    Arregla accesibilidad de link en modo claro para Leer mas en el inicio

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts }) {
                       <div className="text-base font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          className="text-primary-700 hover:text-primary-600 dark:text-primary-500 dark:hover:text-primary-400"
                           aria-label={`Read "${title}"`}
                         >
                           Leer Más &rarr;
